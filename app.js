044bf9cc2234371580b61da01059c809db1f8765
Curtis Watkins <jwatkin5@terpmail.umd.edu>
2026-05-13 15:54:38 -0400

    added dotenv

--- app.js
+++ app.js
@@ -2,13 +2,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const supabaseClient = require('@supabase/supabase-js')
+const dotenv = require('dotenv');
+
 const app=express();
 const port=3000;
+dotenv.config()
 
-app,use(bodyParser.json())
+app.use(bodyParser.json())
 
-const supabaseUrl = 'https://paodsufsmvtzneuqxqdt.supabase.co';
-const supabaseKey ='sb_publishable_NyCUaUtScUcWGhAv6tJfiQ_2fS7x5AT';
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey =process.env.SUPABASE_KEY;
 const supbabase = supabaseClient.createClient(supabaseUrl,supabaseKey);
 
 app.use(express.static(__dirname + '/public'));
