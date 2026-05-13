bf8a97c15df489dbbe91dd8e579c4b5d342ee98c
Curtis Watkins <jwatkin5@terpmail.umd.edu>
2026-05-13 14:35:18 -0400

    changed some things around, added a app.js that may be removed

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app=express();
+const port=3000;
+app.use(express.static(__dirname + '/public'));
+
+app.get('/hello',(req,res) =>{
+    const output = {
+        phrase: 'Hello world',
+    };
+
+    res.json(output);
+})
+
+app.get('/about', (req,res) =>{
+    res.sendFile('public/about.html',{ root:__dirname });
+})
+
+app.get('/', (req,res) =>{
+    res.sendFile('public/home.html',{ root:__dirname });
+})
+
+app.get('/results', (req,res) =>{
+    res.sendFile('public/results.html',{ root:__dirname });
+})
+
+app.get('/compare', (req,res) =>{
+    res.sendFile('public/compare.html',{ root:__dirname });
+})
+
+app.listen(port, () => {
+    console.log("Express app is listening on port: ${port}");
+});
--- index.js
+++ index.js
@@ -1,66 +1,18 @@
 const express = require("express");
-const cors = require("cors");
-const fetch = require("node-fetch");
-
+const http = require('http');
+const hostName = '127.0.0.1';
 const app = express();
 const port = 3000;
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static(__dirname +'/public'));
 
-app.get("/api/search", async (req, res) => {
+app.listen(port,()=>{
+    console.log('Express app listening on port: ${port}');
+})
 
-    const query = req.query.query;
-
-    try {
-
-        const response = await fetch(
-            `https://world.openfoodfacts.org/cgi/search.pl?search_terms=${query}&search_simple=1&action=process&json=1`
-        );
-
-        const data = await response.json();
-
-        res.json(data.products.slice(0, 10));
-
-    } catch (err) {
-
-        console.log(err);
-
-        res.status(500).json({
-            error: "Search failed"
-        });
-    }
-});
-
-
-app.get("/api/product/:code", async (req, res) => {
-
-    const code = req.params.code;
-
-    try {
-
-        const response = await fetch(
-            `https://world.openfoodfacts.org/api/v0/product/${code}.json`
-        );
-
-        const data = await response.json();
-
-        res.json(data.product);
-
-    } catch (err) {
-
-        res.status(500).json({
-            error: "Product failed"
-        });
-    }
-});
-
-const http = require('http');
-const hostName = '127.0.0.1';
 const server =http.createServer((req,res) =>{});
 server.listen(PORT, hostName, () => {
     console.log(`Server running on http://${hostName}:${port}`);
 });
-
-module.exports = app;
